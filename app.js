98d6a43e857306480bcf2444a30d9a846b9cceed
gab <155622983+GabGaitanidis@users.noreply.github.com>
2024-07-25 15:36:48 +0300

    #

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const testRoute = require("./routes/home");
+const testRoute = require("./routes/indexRoute");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -18,7 +18,9 @@ const messages = [
 router.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
-
+router.get("/new", (req, res) => {
+  res.render("form", { messages: messages });
+});
 router.post("/", (req, res) => {
   messages.push({
     text: req.body.messageText,
