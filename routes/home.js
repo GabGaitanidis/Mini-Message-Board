5e38c6a5f3d6789af5df15df83285ae182e782c3
gab <155622983+GabGaitanidis@users.noreply.github.com>
2024-07-25 14:33:02 +0300

    Added style and layout

--- routes/home.js
+++ routes/home.js
@@ -18,10 +18,8 @@ const messages = [
 router.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
-router.get("/new", (req, res) => {
-  res.render("form");
-});
-router.post("/new", (req, res) => {
+
+router.post("/", (req, res) => {
   messages.push({
     text: req.body.messageText,
     user: req.body.messageUser,
